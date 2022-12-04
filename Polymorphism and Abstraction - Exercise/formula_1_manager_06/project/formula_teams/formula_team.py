4309b2f06768cb2dd66ef654cf8a2d097a4c217e
Alexander Zashev <azashev93@gmail.com>
2022-12-04 18:33:27 +0200

    Add Formula 1 Manager

---
+++ Polymorphism and Abstraction - Exercise/formula_1_manager_06/project/formula_teams/formula_team.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+
+class FormulaTeam(ABC):
+    def __init__(self, budget: int):
+        self.budget = budget
+
+    @property
+    def budget(self):
+        return self.__budget
+
+    @budget.setter
+    def budget(self, value):
+        if value < 1_000_000:
+            raise ValueError("F1 is an expensive sport, find more sponsors!")
+        self.__budget = value
+    
+    @property
+    @abstractmethod
+    def sponsors(self):
+        pass
+
+    @property
+    @abstractmethod
+    def expenses_for_race(self):
+        pass
+
+    def calculate_revenue_after_race(self, race_pos: int):
+        revenue = 0
+        for positions in self.sponsors.values():
+            for pos, value in positions.items():
+                if race_pos <= pos:
+                    revenue += value
+                    break
+        revenue -= self.expenses_for_race
+        self.budget += revenue
+        return f"The revenue after the race is {revenue}$. Current budget {self.budget}$"
